68338877e26edf0dddf77309052abcd0f85b49ed
Christian Bouwense <bouwensec@gmail.com>
2023-12-28 21:17:39 -0500

    remove ts

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+function addStar(scene) {
+    const geometry = new THREE.SphereGeometry(0.1, 12, 12);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(30));
+    star.position.set(x, y, z);
+    scene.add(star);
+}
 
 function main() {
     const canvas = document.querySelector("#c");
@@ -9,18 +20,27 @@ function main() {
     const fov = 90;
     const aspect = window.innerWidth / window.innerHeight;
     const near = 0.1;
-    const far = 30;
+    const far = 300;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 2;
+    camera.position.x = 4;
+    camera.position.y = 2;
+    camera.position.z = 3;
 
-    // Set up cube
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-    const material = new THREE.MeshStandardMaterial({color: 0x44aa88});
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    // Load up the pizza box boys
+    const loader = new GLTFLoader();
+    let pizzaBox;
+    loader.load(
+        'source/pizza.glb',
+        (gltf) => {
+            pizzaBox = gltf.scene;
+            scene.add(gltf.scene);
+        },
+        undefined,
+        (error) => console.error('An error occurred while loading the model:', error)
+    );
+
+    // Set up stars
+    Array(200).fill().forEach(() => addStar(scene));
 
     // Set up renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true, canvas,  });
@@ -28,29 +48,29 @@ function main() {
     renderer.render(scene, camera);
 
     // Set up lighting
-    const pointLight = new THREE.PointLight(0xffffff)
-    pointLight.position.set(2, 2, 2);
-    scene.add(pointLight);
+    // const pointLight = new THREE.PointLight(0xffffff)
+    // pointLight.position.set(1.01, 1.01, 1.01);
+    // scene.add(pointLight);
 
-    const ambientLight = new THREE.AmbientLight(0xefefef);
+    const ambientLight = new THREE.AmbientLight(0xffffff);
     scene.add(ambientLight);
 
-    const lightHelper = new THREE.PointLightHelper(pointLight);
-    scene.add(lightHelper);
+    // const lightHelper = new THREE.PointLightHelper(pointLight);
+    // scene.add(lightHelper);
 
     // Set up grid
-    const gridHelper = new THREE.GridHelper(200, 50);
-    scene.add(gridHelper);
+    // const gridHelper = new THREE.GridHelper(200, 50);
+    // scene.add(gridHelper);
 
     // Set up controls
     const controls = new OrbitControls(camera, renderer.domElement);
 
     function loop(time) {
         { // Update state
-            time *= 0.001;  // convert time to seconds
+            time *= 0.0003;  // convert time to seconds
         
-            cube.rotation.x = time;
-            cube.rotation.y = time;
+            if (pizzaBox) pizzaBox.rotation.y = time;
+            // cube.rotation.y = time;
         }
 
         { // Update view
